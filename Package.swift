557d55b588188d1d3eb4070760586864331b7718
Dubon Ya'ar <10558287+DubonYaar@users.noreply.github.com>
2025-11-29 09:35:36 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sliders",
-            dependencies: ["CGExtender", "Shapes", "bez"]),
+            dependencies: [/*"CGExtender",*/ "Shapes"/*, "bez"*/]),
         .testTarget(
             name: "SlidersTests",
             dependencies: ["Sliders"]),
